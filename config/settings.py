3b864125280a50df0cc909494b5e5f6fdbf3b56f
HerrMaszewsk <tadmaszewski@gmail.com>
2025-06-30 12:58:43 +0200

    libgdal fix 7 ?

--- config/settings.py
+++ config/settings.py
@@ -73,11 +73,12 @@ TEMPLATES = [
 # BAZA DANYCH
 DATABASES = {
     'default': dj_database_url.config(
-        default=config("DATABASE_URL", default="postgres://user:pass@localhost:5432/dbname"),
+        default=config("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True
     )
 }
+
 # WALIDACJA HASEŁ
 AUTH_PASSWORD_VALIDATORS = [
     {
