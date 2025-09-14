d86262bf728da8174986c2e2d841118a366b7e60
HerrMaszewsk <tadmaszewski@gmail.com>
2025-09-14 13:47:17 +0200

    ikony, strona o projekcie

--- mapa/urls.py
+++ mapa/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from .views import map_view
 
 urlpatterns = [
     path('map/', map_view, name='map'),
+    path('about/', TemplateView.as_view(template_name='mapa/about.html'), name='about'),
 ]
